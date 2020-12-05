b2b64d08fbffc1674c4263b01acca79b95709429
jhengyilin <75511000+jhengyilin@users.noreply.github.com>
2020-12-05 11:04:53 +0800

    Update cpp

--- midterm-project.cpp
+++ midterm-project.cpp
@@ -18,5 +18,28 @@ struct Machine {
 
 
 int main() {
+
+    int machineNum = -1, taskNum = -1, maxRepairNum = -1;
+
+    cin>> machineNum>>taskNum>>maxRepairNum;
+
+    Machine machine[machineNum] = {0};
+
+    for(int i = 0; i < machineNum;, i++)
+    {
+        machine[i].id = i;
+        cin >> machine[i].idealProduction >> machine.[i].decreasingRate
+        >> machine.[i].maintenanceTime >> machine[i].initialProduction
+         >> machine.[i].minimumProduction;
+    }
+
+    Task task[taskNum] = {0};
+
+    for(int i = 0; i < taskNum;, i++)
+    {
+        task[i].id = i;
+        cin >> task[i].quantity>> task[i].deadline;
+    }
+
   return 0;
 }
